366412730637d9f0507c5a3c9ae24087b9394b0a
Luis Fernando Montes Blanco <luisfer.montesblanco@Outlook.com>
2023-11-06 17:41:12 -0600

    new navbar component

---
+++ components/navbar.js
@@ -0,0 +1,41 @@
+function MiComponente(elementoPadre) {
+    this.elementoPadre = elementoPadre;
+    this.elementoHijo = document.createElement('div');
+    const username="usuario 1";
+    const userImg="../assets/backgorund.jpg"
+
+    this.elementoHijo.innerHTML = 
+    `
+        <ul class="nav-ul">
+            <li class="user-li">
+                <a href="#">
+                    <img class="img-user" src="${userImg}">
+                    ${username}
+                </a>
+            </li>
+            <li class="search-li">
+                <a href="#">
+                    <img src="../assets/ant-design_search-outlined.svg">
+                </a>
+            </li>
+
+            <li>
+                <a href="#">Series</a>
+            </li>
+
+            <li>
+                <a href="#">Movies</a>
+            </li>
+
+            <li>
+                <a href="#">My List</a>
+            </li>
+        </ul>
+    `;
+    this.elementoPadre.appendChild(this.elementoHijo);
+  }
+  
+  // Para usar el componente:
+  const contenedor = document.getElementById('main-nav');
+  const miComponente = new MiComponente(contenedor);
+  
