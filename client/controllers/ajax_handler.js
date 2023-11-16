7635acbfe26e3388a3ba18f7fdd35d1b8f0a0842
unknown <isaac.noriega@iteso.mx>
2023-11-15 20:15:18 -0600

    Añadi el user_uitls , las dependecias necesarias

--- client/controllers/ajax_handler.js
+++ client/controllers/ajax_handler.js
@@ -5,3 +5,9 @@ async function loadMovies (url) {
     if (response.status != 200) return [];
     return await response.json();
 }
+
+async function loadUser(url){
+    let response = await fetch(url);
+    if (response.status != 200) return [];
+    return await response.json();
+}
